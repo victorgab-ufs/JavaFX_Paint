26fa80ec82361b81f77fd8b29e4129f2d5c43856
Renato Vasconcelos Campos Filho <renatovascfilho@hotmail.com>
2026-06-14 18:52:43 -0300

    ícone do aplicativo

--- src/main/java/paint/App.java
+++ src/main/java/paint/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -15,6 +16,9 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("visaoPaint"));
+        stage.setTitle("LinkedPaint");
+        Image icone = new Image(App.class.getResourceAsStream("linkedPaint_logo.png"));
+        stage.getIcons().add(icone);
         stage.setScene(scene);
         stage.show();
     }
